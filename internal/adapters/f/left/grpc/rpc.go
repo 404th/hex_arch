dbfab5f0d001c0c6635f7e3a6d31ac61e4254c1e
404th <umarov.doniyor.2002@gmail.com>
2022-03-12 00:21:20 +0500

    gRPC has been implemented

--- internal/adapters/f/left/grpc/rpc.go
+++ internal/adapters/f/left/grpc/rpc.go
@@ -1,1 +1,76 @@
 package grpc
+
+import (
+	"context"
+	"errors"
+
+	"github.com/404th/hex_arch/adapters/f/left/grpc/pb"
+)
+
+func (ga Adapter) GetAddition(ctx context.Context, req *pb.OperationDetails) (*pb.Answer, error) {
+	var response *pb.Answer
+
+	if req.GetA() == 0 || req.GetB() == 0 {
+		return response, errors.New("Unsatisfied inputs!")
+	}
+
+	result, err := ga.api.GetAddition(req.GetA(), req.GetB())
+	if err != nil {
+		return response, err
+	}
+
+	response.Result = result
+
+	return response, nil
+}
+
+func (ga Adapter) GetSubtraction(ctx context.Context, req *pb.OperationDetails) (*pb.Answer, error) {
+	var response *pb.Answer
+
+	if req.GetA() == 0 || req.GetB() == 0 {
+		return response, errors.New("Unsatisfied inputs!")
+	}
+
+	result, err := ga.api.GetSubtraction(req.GetA(), req.GetB())
+	if err != nil {
+		return response, err
+	}
+
+	response.Result = result
+
+	return response, nil
+}
+
+func (ga Adapter) GetMultiplication(ctx context.Context, req *pb.OperationDetails) (*pb.Answer, error) {
+	var response *pb.Answer
+
+	if req.GetA() == 0 || req.GetB() == 0 {
+		return response, errors.New("Unsatisfied inputs!")
+	}
+
+	result, err := ga.api.GetMultiplication(req.GetA(), req.GetB())
+	if err != nil {
+		return response, err
+	}
+
+	response.Result = result
+
+	return response, nil
+}
+
+func (ga Adapter) GetDivision(ctx context.Context, req *pb.OperationDetails) (*pb.Answer, error) {
+	var response *pb.Answer
+
+	if req.GetA() == 0 || req.GetB() == 0 {
+		return response, errors.New("Unsatisfied inputs!")
+	}
+
+	result, err := ga.api.GetDivision(req.GetA(), req.GetB())
+	if err != nil {
+		return response, err
+	}
+
+	response.Result = result
+
+	return response, nil
+}
--- internal/adapters/f/left/grpc/server.go
+++ internal/adapters/f/left/grpc/server.go
@@ -1,1 +1,30 @@
 package grpc
+
+import (
+	"net"
+
+	"github.com/404th/hex_arch/adapters/f/left/grpc/pb"
+	"github.com/404th/hex_arch/ports"
+	"google.golang.org/grpc"
+)
+
+type Adapter struct {
+	api ports.APIPorts
+}
+
+func (sv *Adapter) Run(port string) error {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		return err
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterArithmeticServiceServer(grpcServer, sv.api)
+
+	if err = grpcServer.Serve(lis); err != nil {
+		return err
+	}
+
+	return nil
+}
