fb0a2f5c8f81f1c292d77157613ad73266a27c92
404th <umarov.doniyor.2002@gmail.com>
2022-03-13 15:41:29 +0500

    some changes

--- internal/adapters/app/api/api.go
+++ internal/adapters/app/api/api.go
@@ -1,6 +1,8 @@
 package api
 
-import "github.com/404th/hex_arch/internal/ports"
+import (
+	"github.com/404th/hex_arch/internal/ports"
+)
 
 type Adapter struct {
 	arith ports.ArithPorts
@@ -17,12 +19,20 @@ func (apia *Adapter) GetAddition(a, b int32) (int32, error) {
 		return 0, err
 	}
 
+	if err = apia.db.AddToHistory(result, "addition"); err != nil {
+		return 0, err
+	}
+
 	return result, nil
 }
 
 func (apia *Adapter) GetSubtraction(a, b int32) (int32, error) {
 	result, err := apia.arith.Subtraction(a, b)
 	if err != nil {
+		return 0, err
+	}
+
+	if err = apia.db.AddToHistory(result, "subtraction"); err != nil {
 		return 0, err
 	}
 
@@ -35,12 +45,20 @@ func (apia *Adapter) GetMultiplication(a, b int32) (int32, error) {
 		return 0, err
 	}
 
+	if err = apia.db.AddToHistory(result, "multiplication"); err != nil {
+		return 0, err
+	}
+
 	return result, nil
 }
 
 func (apia *Adapter) GetDivision(a, b int32) (int32, error) {
 	result, err := apia.arith.Division(a, b)
 	if err != nil {
+		return 0, err
+	}
+
+	if err = apia.db.AddToHistory(result, "division"); err != nil {
 		return 0, err
 	}
 
